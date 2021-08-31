f0ef840e829689e0549b910569bd5a8bbef5bbc7
androidDevVivek <vivek.kumar@Krazybee.com>
2021-08-31 14:01:09 +0530

    Changing file path

--- app/src/main/java/com/krazybee/githubactions/MainActivity.kt
+++ app/src/main/java/com/krazybee/githubactions/MainActivity.kt
@@ -7,7 +7,6 @@ class MainActivity : AppCompatActivity() {
     companion object{
         const val TAG="MainActivity"
     }
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
