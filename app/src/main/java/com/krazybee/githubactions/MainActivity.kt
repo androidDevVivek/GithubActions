bba5cb7f6f2c8d2cc9b29c8e39056816e6258214
androidDevVivek <vivek.kumar@Krazybee.com>
2021-08-31 16:11:10 +0530

    Added Onstart

--- app/src/main/java/com/krazybee/githubactions/MainActivity.kt
+++ app/src/main/java/com/krazybee/githubactions/MainActivity.kt
@@ -1,14 +1,22 @@
 package com.krazybee.githubactions
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
-    companion object{
-        const val TAG="MainActivity"
+    companion object {
+        const val TAG = "MainActivity"
     }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        Log.d(TAG, "onCreate: ")
+    }
+
+    override fun onStart() {
+        super.onStart()
+        Log.d(TAG, "onStart: ")
     }
 }
