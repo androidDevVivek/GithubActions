a2e1e5bd4d8b7dd9539883036a1f47639c69c6f5
androidDevVivek <vivek.kumar@Krazybee.com>
2021-08-31 13:47:32 +0530

    Added TAG.

--- app/src/main/java/com/krazybee/githubactions/MainActivity.kt
+++ app/src/main/java/com/krazybee/githubactions/MainActivity.kt
@@ -4,9 +4,11 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
 class MainActivity : AppCompatActivity() {
+    companion object{
+        const val TAG="MainActivity"
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
         setContentView(R.layout.activity_main)
     }
 }
