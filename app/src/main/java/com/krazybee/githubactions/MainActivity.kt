0fb6a3c71256c63cfdd387211294d8805e321464
androidDevVivek <vivek.kumar@Krazybee.com>
2021-09-01 14:32:53 +0530

    Added toast to 1.2

--- app/src/main/java/com/krazybee/githubactions/MainActivity.kt
+++ app/src/main/java/com/krazybee/githubactions/MainActivity.kt
@@ -1,11 +1,13 @@
 package com.krazybee.githubactions
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        Toast.makeText(this, "1.2", Toast.LENGTH_LONG).show()
     }
 }
