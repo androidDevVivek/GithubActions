277a71cfb3b6017e62fb2d92435fe5ab1997aa67
androidDevVivek <vivek.kumar@Krazybee.com>
2021-09-01 14:31:14 +0530

    Added toast to master

--- app/src/main/java/com/krazybee/githubactions/MainActivity.kt
+++ app/src/main/java/com/krazybee/githubactions/MainActivity.kt
@@ -1,11 +1,13 @@
 package com.krazybee.githubactions
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        Toast.makeText(this, "Master", Toast.LENGTH_LONG).show()
     }
 }
